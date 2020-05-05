8b87d7262ca465f68360e230de39ca3ba88db753
Tim Duffy <tduffy@appliedlogix.com>
2020-05-04 20:20:10 -0400

    initial version that returns temp by lat/lng in F or C

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pytemp
-Get's the temperature based on US location information
+Gets the temperature based on US location information
+
+Dependencies:
+
+    pip install requests
+
+Usage:
+
+    python3 pytemp.py <lat> <lng>
+
+This uses the weather.gov html web end point.  Please use it responsibly.
---
+++ pytemp.py
@@ -0,0 +1,46 @@
+
+import sys
+import requests
+
+def _get_text(lat, lng):
+    url = 'https://forecast.weather.gov/MapClick.php?lat=%s&lon=%s' % (str(lat), str(lng))
+    text = None
+    try:
+        r = requests.get(url, json={})
+        text = r.text
+        #print(r.text)
+        #raise Exception('debug')
+    except Exception as ex:
+        print("[ERRPR] request error: %s", str(ex))
+    return text
+
+def _parse_text(text):
+    parts = text.split('<p class="myforecast-current-lrg">')
+    temp = parts[1].split('</p>')[0]
+    current_temp = temp.split('&')[0]
+    return current_temp
+
+def get_temp_in_f(lat, lng):
+    text = _get_text(43.0374, -77.7038) 
+    current_temp = _parse_text(text)
+    return int(float(current_temp))
+
+def get_temp_in_c(lat, lng):
+    text = _get_text(43.0374, -77.7038) 
+    current_temp = _parse_text(text)
+    temp = int(float(current_temp))
+    return int( (temp - 32) * (5.0/9.0) )
+
+if __name__ == '__main__':
+
+    if len( sys.argv ) != 3:
+        print("Usage:\n\n\tpython pytemp.py <lat> <lng>\n\n")
+    else:
+
+        lat = sys.argv[1]
+        lng = sys.argv[2]
+
+        f = get_temp_in_f(lat, lng)
+        c = get_temp_in_c(lat, lng)
+        
+        print("Temp: %i\xb0F ( %i\xb0C )" % (f, c))
